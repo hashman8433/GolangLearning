9725aaefc96b0d9e76b274216416d8fd9178cbd1
root1 <843355856@qq.com>
2018-08-22 23:02:20 +0800

    add chapter4 even(lib) fmt

---
+++ chapter4/genaral.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	str := "message"
+	fmt.Printf("%v\n", str)
+	fmt.Printf("%#v\n", str)
+	fmt.Printf("%T\n", str)
+
+
+}
---
+++ chapter4/myeven.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"even"
+)
+
+func main() {
+	i := 5
+	fmt.Println("Is", i, "evem? ", even.Even(i))
+}
+
+
